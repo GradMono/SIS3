ee344e4212a95f73a933ca9ce6dfc2bdd330aed0
sniper4627 <joshua_apellido@yahoo.com>
2018-08-13 17:03:57 +0800

    ne

---
+++ src/sis3/Connect.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sis3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author Jerone
+ */
+public class Connect {
+    Connection conn=null;
+    
+    
+    public static Connection ConnectDb(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/accounts","root","");
+             JOptionPane.showMessageDialog(null,"Connection Established");
+            return conn;
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+            
+            
+        }
+        }
+    
+}
